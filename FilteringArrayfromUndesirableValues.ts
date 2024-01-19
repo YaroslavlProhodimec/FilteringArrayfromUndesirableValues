bdf86242703e0f9e2eb831870feeea5db5812b01
TonyMyrato <tonymyrato@TonyMyratos-Air.MyMiFi>
2024-01-19 12:44:12 -0800

    new branch task

---
+++ FilteringArrayfromUndesirableValues.ts
@@ -0,0 +1,13 @@
+/**
+  * Описание задачи: Напишите функцию, которая очищает массив от нежелательных значений, таких как false, undefined, пустые строки, ноль, null.
+  * Ожидаемый результат: [0, 1, false, 2, undefined, '', 3, null] => [1, 2, 3]
+  * Сложность задачи: 1 of 5
+  * @param {Array} array - Массив любых элементов
+  * @returns {Array}
+*/
+const compact = (array) => {
+ throw new Error('Напишите здесь свое решение');
+}
+
+const data = [0, 1, false, 2, undefined, '', 3, null];
+console.log(compact(data)) // [1, 2, 3]
